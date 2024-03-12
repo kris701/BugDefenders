6f4b3c3826dcdc4d2690f309f7cadee13cd03a07
Kristian Skov Johansen <kris701kj@gmail.com>
2024-03-12 20:43:15 +0100

    Added a button to open the game page in itch.io

--- BugDefender.OpenGL/Views/MainMenuView/MainMenuView.design.cs
+++ BugDefender.OpenGL/Views/MainMenuView/MainMenuView.design.cs
@@ -2,6 +2,7 @@
 using BugDefender.OpenGL.Engine;
 using BugDefender.OpenGL.Engine.Controls;
 using BugDefender.OpenGL.Engine.Helpers;
+using BugDefender.OpenGL.Helpers;
 using BugDefender.OpenGL.Views;
 using Microsoft.Xna.Framework;
 using System;
@@ -152,6 +153,18 @@ namespace BugDefender.OpenGL.Screens.MainMenu
                 Font = BasicFonts.GetFont(24),
                 FontColor = Color.White,
                 Text = "Users",
+                FillColor = Parent.TextureController.GetTexture(new Guid("aa60f60c-a792-425b-a225-5735e5a33cc9")),
+                FillClickedColor = Parent.TextureController.GetTexture(new Guid("12a9ad25-3e34-4398-9c61-6522c49f5dd8")),
+            });
+            AddControl(0, new BugDefenderButtonControl(Parent, clicked: (x) => LinkHelper.OpenUrl("https://kris701.itch.io/bug-defenders"))
+            {
+                X = 1700,
+                Y = 980,
+                Width = 200,
+                Height = 50,
+                Font = BasicFonts.GetFont(24),
+                FontColor = Color.White,
+                Text = "Itch IO",
                 FillColor = Parent.TextureController.GetTexture(new Guid("aa60f60c-a792-425b-a225-5735e5a33cc9")),
                 FillClickedColor = Parent.TextureController.GetTexture(new Guid("12a9ad25-3e34-4398-9c61-6522c49f5dd8")),
             });
