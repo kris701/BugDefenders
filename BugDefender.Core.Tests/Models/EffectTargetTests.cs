ab871d1fc4027bce97e0f4ca22df31bb694f829a
Kristian Skov Johansen <kris701kj@gmail.com>
2024-03-22 13:52:41 +0100

    Refactoring

---
+++ BugDefender.Core.Tests/Models/EffectTargetTests.cs
@@ -0,0 +1,96 @@
+﻿using BugDefender.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BugDefender.Core.Tests.Models
+{
+    [TestClass]
+    public class EffectTargetTests
+    {
+        [TestMethod]
+        public void Can_SetModifier()
+        {
+            // ARRANGE
+            var target = new EffectTarget("prop");
+            var modifier = 2;
+
+            // ACT
+            target.Modifier = modifier;
+
+            // ASSERT
+            Assert.AreEqual(modifier, target.Modifier);
+            Assert.IsNull(target.Value);
+        }
+
+        [TestMethod]
+        public void Can_SetValue()
+        {
+            // ARRANGE
+            var target = new EffectTarget("prop");
+            var value = new JsonElement();
+
+            // ACT
+            target.Value = value;
+
+            // ASSERT
+            Assert.AreEqual(value, target.Value);
+            Assert.AreEqual(1, target.Modifier);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Cannot have a modifier and value at the same time!")]
+        public void Cant_SetValueAndModifier_1()
+        {
+            // ARRANGE
+            var target = new EffectTarget("prop");
+
+            // ACT
+            target.Value = new JsonElement();
+            target.Modifier = 2;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Cannot have a modifier and value at the same time!")]
+        public void Cant_SetValueAndModifier_2()
+        {
+            // ARRANGE
+            var target = new EffectTarget("prop");
+
+            // ACT
+            target.Modifier = 2;
+            target.Value = new JsonElement();
+        }
+
+        [TestMethod]
+        public void Can_Copy_1()
+        {
+            // ARRANGE
+            var target = new EffectTarget("prop");
+            target.Value = new JsonElement();
+
+            // ACT
+            var copy = target.Copy();
+
+            // ASSERT
+            Assert.IsTrue(target.Equals(copy));
+        }
+
+        [TestMethod]
+        public void Can_Copy_2()
+        {
+            // ARRANGE
+            var target = new EffectTarget("prop");
+            target.Modifier = 2;
+
+            // ACT
+            var copy = target.Copy();
+
+            // ASSERT
+            Assert.IsTrue(target.Equals(copy));
+        }
+    }
+}
--- BugDefender.Core/Models/EffectTarget.cs
+++ BugDefender.Core/Models/EffectTarget.cs
@@ -1,12 +1,34 @@
-﻿using System.Text.Json;
+﻿using System.Collections;
+using System.Reflection;
+using System.Text.Json;
 
 namespace BugDefender.Core.Models
 {
     public class EffectTarget
     {
         public string Target { get; set; }
-        public JsonElement? Value { get; set; } = null;
-        public float Modifier { get; set; } = 1;
+        private JsonElement? _value = null;
+        public JsonElement? Value
+        {
+            get => _value;
+            set
+            {
+                if (Modifier != 1 && value != null)
+                    throw new Exception("Cannot have a modifier and value at the same time!");
+                _value = value;
+            }
+        }
+        private float _modifier = 1;
+        public float Modifier
+        {
+            get => _modifier;
+            set
+            {
+                if (Value != null && value != 1)
+                    throw new Exception("Cannot have a modifier and value at the same time!");
+                _modifier = value;
+            }
+        }
 
         public EffectTarget(string target)
         {
@@ -39,6 +61,156 @@ namespace BugDefender.Core.Models
             }
             _displayName = _displayName.Trim();
             return _displayName;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is EffectTarget other)
+            {
+                if (other.Target != Target) return false;
+                if (other.Modifier != Modifier) return false;
+                if (!other.Value.Equals(Value)) return false;
+                return true;
+            }
+            return false;
+        }
+
+        public override int GetHashCode() => HashCode.Combine(Target, Value, Modifier);
+
+        public void ApplyUpgradeEffectOnObject<U>(U item) where U : notnull => ExecuteActionOnProperty(item, ApplyEffect);
+        public void UnApplyUpgradeEffectOnObject<U>(U item) where U : notnull => ExecuteActionOnProperty(item, UnApplyEffect);
+        public void TryApplyUpgradeEffectOnObject<U>(U item) where U : notnull => TryExecuteActionOnProperty(item, ApplyEffect);
+        public void TryUnApplyUpgradeEffectOnObject<U>(U item) where U : notnull => TryExecuteActionOnProperty(item, UnApplyEffect);
+
+        private void ApplyEffect<U>(U item, PropertyInfo first)
+        {
+            var current = first.GetValue(item);
+            if (current == null)
+                throw new Exception("Unknown change attempted on object!");
+            if (Value != null)
+            {
+                var newValue = JsonSerializer.Deserialize((dynamic)Value, current.GetType());
+                first.SetValue(item, newValue);
+            }
+            else if (Modifier != 1)
+            {
+                var asFloat = Convert.ToSingle(current);
+                first.SetValue(item, Convert.ChangeType(asFloat * Modifier, current.GetType()));
+            }
+            else
+                throw new Exception("Unknown change attempted on object!");
+        }
+
+        private void UnApplyEffect<U>(U item, PropertyInfo first)
+        {
+            var current = first.GetValue(item);
+            if (current == null)
+                throw new Exception("Unknown change attempted on object!");
+            if (Modifier != 1)
+            {
+                var asFloat = Convert.ToSingle(current);
+                first.SetValue(item, Convert.ChangeType(asFloat / Modifier, current.GetType()));
+            }
+            else
+                throw new Exception("Unknown change attempted on object!");
+        }
+
+        private void ExecuteActionOnProperty<U>(U item, Action<object, PropertyInfo> action) where U : notnull
+        {
+            var target = GetPropertyInstance(item);
+            if (target == null)
+                throw new Exception("Invalid object selection!");
+            action(target.Item1, target.Item2);
+        }
+
+        private void TryExecuteActionOnProperty<U>(U item, Action<object, PropertyInfo> action) where U : notnull
+        {
+            var target = GetPropertyInstance(item);
+            if (target == null)
+                return;
+            action(target.Item1, target.Item2);
+        }
+
+        private Tuple<object, PropertyInfo>? GetPropertyInstance<U>(U item) where U : notnull
+        {
+            if (Target.Count(x => x == '[' || x == ']') % 2 != 0)
+                return null;
+
+            int seekIndex = 0;
+            string parsed = "";
+            PropertyInfo? targetInfo = null;
+            object? targetObject = item;
+            var propInfo = targetObject.GetType().GetProperties();
+            while (seekIndex < Target.Length)
+            {
+                if (Target[seekIndex] == '.')
+                {
+                    targetInfo = propInfo.FirstOrDefault(x => x.Name == parsed);
+                    if (targetInfo == null)
+                        return null;
+                    targetObject = targetInfo.GetValue(targetObject);
+                    if (targetObject == null)
+                        return null;
+                    propInfo = targetObject.GetType().GetProperties();
+                    parsed = "";
+                }
+                else if (Target[seekIndex] == '[')
+                {
+                    targetInfo = propInfo.FirstOrDefault(x => x.Name == parsed);
+                    if (targetInfo == null)
+                        return null;
+                    targetObject = targetInfo.GetValue(targetObject);
+                    if (targetObject == null)
+                        return null;
+                    propInfo = targetObject.GetType().GetProperties();
+
+                    var within = Target.Substring(seekIndex + 1, Target.IndexOf(']', seekIndex) - seekIndex - 1);
+                    var split = within.Split('=');
+                    var targetProp = split[0];
+                    var targetValue = split[1];
+
+                    if (targetObject is IEnumerable enu)
+                    {
+                        bool any = false;
+                        foreach (var inner in enu)
+                        {
+                            var listPropInfo = inner.GetType().GetProperties();
+                            var listTargetInfo = listPropInfo.FirstOrDefault(x => x.Name == targetProp);
+                            if (listTargetInfo == null)
+                                continue;
+                            var listTargetObject = listTargetInfo.GetValue(inner);
+                            if (listTargetObject == null)
+                                continue;
+                            if (listTargetObject.ToString() == targetValue)
+                            {
+                                targetInfo = null;
+                                propInfo = listPropInfo;
+                                targetObject = inner;
+                                any = true;
+                                break;
+                            }
+                        }
+                        if (!any)
+                            return null;
+                    }
+                    else
+                        return null;
+
+                    parsed = "";
+                    seekIndex += within.Length + 2;
+                }
+                else
+                    parsed += Target[seekIndex];
+
+                seekIndex++;
+            }
+
+            if (parsed != "")
+                targetInfo = propInfo.FirstOrDefault(x => x.Name == parsed);
+
+            if (targetObject != null && targetInfo != null)
+                return new Tuple<object, PropertyInfo>(targetObject, targetInfo);
+            return null;
         }
     }
 }
